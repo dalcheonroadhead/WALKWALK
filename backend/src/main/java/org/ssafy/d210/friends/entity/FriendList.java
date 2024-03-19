86844cac90a5858ee1a9f1d845a1fc8c97ca3c24
sehyun <germs1020@gmail.com>
2024-03-20 05:27:38 +0900

    feat: friendList entity 구현

--- backend/src/main/java/org/ssafy/d210/friends/entity/FriendList.java
+++ backend/src/main/java/org/ssafy/d210/friends/entity/FriendList.java
@@ -3,15 +3,25 @@ package org.ssafy.d210.friends.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.ssafy.d210.members.entity.Members;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 public class FriendList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 1)
     @Column(name = "friend_list_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private Members senderId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private Members receiverId;
+
+    @Column(name = "is_accepted", nullable = false)
+    private Boolean isAccepted = false;
 }
