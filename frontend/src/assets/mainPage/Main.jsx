4acdf9b9d034bdabce522fb996eafdcbb84e8127
rlarbfl0702 <rlarbfl0702@naver.com>
2024-03-19 17:11:34 +0900

    feat: 메인화면 이번주 기록 화면 구현, 그래프 추가

--- frontend/src/assets/mainPage/Main.jsx
+++ frontend/src/assets/mainPage/Main.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
 import styles from "./Main.module.css";
+import { ResponsiveBar } from "@nivo/bar";
 
 const Main = function(){
+    const handle = {
+        barClick: (data) => {
+            console.log(data);
+        },
+
+        legendClick: (data) => {
+            console.log(data);
+        },
+    };
 
     const [tabIndex, settabIndex] = useState(0);
 
@@ -113,8 +123,167 @@ const Main = function(){
                             <p className={styles.week_avg_number}>6704</p>
                             <p className={styles.week_avg_walk}>걸음</p>
                         </div>
-                        <div>
-
+                        <div className={styles.graph_container}>
+                            <div className={styles.graph_back}></div>
+                            <div className={styles.graph_style}>
+                            <ResponsiveBar /**
+                         * chart에 사용될 데이터
+                         */
+                                data={[
+                                    {
+                                        "요일": "월",
+                                        "걸음 수": 6204,
+                                        "걸음 수 색": "#5F5745",
+                                      },
+                                      {
+                                        "요일": "화",
+                                        "걸음 수": 7345,
+                                        "걸음 수 색": "#5F5745",
+                                      },
+                                      {
+                                        "요일": "수",
+                                        "걸음 수": 3234,
+                                        "걸음 수 색": "#5F5745",
+                                      },
+                                      {
+                                        "요일": "목",
+                                        "걸음 수": 2311,
+                                        "걸음 수 색": "#5F5745",
+                                      },
+                                      {
+                                        "요일": "금",
+                                        "걸음 수": 0,
+                                        "걸음 수 색": "#5F5745",
+                                      },
+                                      {
+                                        "요일": "토",
+                                        "걸음 수": 0,
+                                        "걸음 수 색": "#5F5745",
+                                      },
+                                      {
+                                        "요일": "일",
+                                        "걸음 수": 0,
+                                        "걸음 수 색": "#5F5745",
+                                      }
+                                ]}
+                                keys={[
+                                    '걸음 수',
+                                ]}
+                                indexBy="요일"
+                                margin={{ top: 80, right: 30, bottom: 50, left: 75 }}
+                                padding={0.4}
+                                valueScale={{ type: 'linear' }}
+                                indexScale={{ type: 'band', round: true }}
+                                colors={['#5F5745']} // 커스텀해서 사용할 때
+                                defs={[
+                                    {
+                                        id: 'dots',
+                                        type: 'patternDots',
+                                        background: 'inherit',
+                                        color: '#38bcb2',
+                                        size: 4,
+                                        padding: 1,
+                                        stagger: true
+                                    },
+                                    {
+                                        id: 'lines',
+                                        type: 'patternLines',
+                                        background: 'inherit',
+                                        color: '#eed312',
+                                        rotation: -45,
+                                        lineWidth: 6,
+                                        spacing: 10
+                                    }
+                                ]}
+                                fill={[
+                                    {
+                                        match: {
+                                            id: 'fries'
+                                        },
+                                        id: 'dots'
+                                    },
+                                    {
+                                        match: {
+                                            id: 'sandwich'
+                                        },
+                                        id: 'lines'
+                                    }
+                                ]}
+                                borderRadius={8}
+                                borderColor={{
+                                    from: 'color',
+                                    modifiers: [
+                                        [
+                                            'darker',
+                                            1.6
+                                        ]
+                                    ]
+                                }}
+                                axisLeft={{
+                                    tickValues: [0, 6000, 10000], // 축에 표시될 값들
+                                    format: value => value.toLocaleString(), // 숫자 포맷팅 함수 (예: 천 단위 구분자)
+                                    text: {
+                                        fontSize: 10,
+                                        fontFamily: "bc_b",
+                                        fill: '#000000',
+                                    },
+                                }}
+                                theme={{
+                                    /**
+                                     * label style (pad에 표현되는 글씨)
+                                     */
+                                    labels: {
+                                        text: {
+                                            fontSize: 14,
+                                            fontFamily: "bc_b",
+                                            fill: '#000000',
+                                        },
+                                    },
+                                    /**
+                                     * legend style (default로 하단에 있는 색상별 key 표시)
+                                     */
+                                    legends: {
+                                        text: {
+                                            fontSize: 12,
+                                            fontFamily: "bc_b",
+                                            fill: '#000000',
+                                        },
+                                    },
+                                    axis: {
+                                        legend: {
+                                            text: {
+                                                fontSize: 8,
+                                                fontFamily: "bc_b",
+                                                fill: '#000000',
+                                            },
+                                        },
+                                        ticks: {
+                                            text: {
+                                                fontSize: 14,
+                                                fontFamily: "bc_b",
+                                                fill: '#000000',
+                                            },
+                                        },
+                                    }
+                                }}
+                                axisTop={null}
+                                axisRight={null}
+                                labelSkipWidth={12}
+                                labelSkipHeight={12}
+                                labelTextColor={{
+                                    from: 'color',
+                                    modifiers: [
+                                        [
+                                            'darker',
+                                            1.6
+                                        ]
+                                    ]
+                                }}
+                                role="application"
+                                ariaLabel="Nivo bar chart demo"
+                                barAriaLabel={e=>e.id+": "+e.formattedValue+" in country: "+e.indexValue}
+                            />
+                            </div>
                         </div>
                     </div>
                 </div>
