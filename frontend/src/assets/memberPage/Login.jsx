dce3ad6b0ba5f383cc8dd53886cd64d5511a5630
qysim <qysim@naver.com>
2024-03-19 17:15:07 +0900

    feat: 로그인 페이지 구현 중 (#17)

--- frontend/src/assets/memberPage/Login.jsx
+++ frontend/src/assets/memberPage/Login.jsx
@@ -1,6 +1,12 @@
+import styles from "./Login.module.css";
+
 const Login = function () {
   return (
-    <div>Login</div>
+    <div>
+      <div className={styles.login_app_name}>walk walk</div>
+      <img src="/imgs/login_logo.png" alt="login logo" />
+      <img src="/imgs/google_login.svg" alt="google login button" />
+    </div>
   )
 }
 
