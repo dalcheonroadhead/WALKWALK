e10b9c8fb50f250439b22facca8f3a1dd7af182a
serot <serotonins023@gmail.com>
2024-03-21 17:23:00 +0900

    feat - 일주일 운동 조회

--- backend/src/main/java/org/ssafy/d210/walk/repository/ExerciseAccRepository.java
+++ backend/src/main/java/org/ssafy/d210/walk/repository/ExerciseAccRepository.java
@@ -1,7 +1,11 @@
 package org.ssafy.d210.walk.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.ssafy.d210.walk.entity.ExerciseAcc;
 
 public interface ExerciseAccRepository extends JpaRepository<ExerciseAcc, Long> {
+    //
+
 }
+
