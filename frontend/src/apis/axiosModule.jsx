8063030fa8d77a0571ba2521c48d93a67624c55e
qysim <qysim@naver.com>
2024-03-22 10:20:33 +0900

    baseURL 수정

--- frontend/src/apis/axiosModule.jsx
+++ frontend/src/apis/axiosModule.jsx
@@ -14,8 +14,8 @@ import axios from "axios";
 // }
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8081/api',
-  // baseURL: 'https://j10d210.p.ssafy.io/api',
+  // baseURL: 'http://localhost:8081/api',
+  baseURL: 'https://j10d210.p.ssafy.io/api',
   // headers: {
   //   'Authorization': token
   // },
