ad508f9ebeff610c4bee93ebeb05255e2fbac31d
qysim <qysim@naver.com>
2024-03-22 10:41:08 +0900

    url 수정

--- frontend/src/apis/memberApi.jsx
+++ frontend/src/apis/memberApi.jsx
@@ -1,9 +1,9 @@
 import { instance } from "./axiosModule";
 
 export const getGoogleToken = async (token) => {
-    const url = import.meta.env.VITE_NODE_ENV === 'production' ? `oauth/callback/google/token/d-t-d?code=${token}` : `oauth/callback/google/token/l-t-l?code=${token}`;
+    const url = import.meta.env.VITE_NODE_ENV === 'production' ? `/oauth/callback/google/token/d-t-d?code=${token}` : `/oauth/callback/google/token/l-t-l?code=${token}`;
     console.log('url : ', url)
     // const res = await instance.post(url)
-    await instance.post(url).then((res) => {console.log(res)}).catch((err) => {console.log(err)})
+    await instance.get(url).then((res) => {console.log(res)}).catch((err) => {console.log(err)})
     // console.log(res.data)
 }
