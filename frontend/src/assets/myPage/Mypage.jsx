bb3d536037a1416c105b3617d716a6181a1ab849
rlarbfl0702 <rlarbfl0702@naver.com>
2024-03-25 16:10:04 +0900

    feat: Zustand로 하단바 페이지 이동 구현

---
+++ frontend/src/assets/myPage/Mypage.jsx
@@ -0,0 +1,12 @@
+
+const Mypage = function(){
+    return(
+        <>
+            <div>
+                <h1>마이 페이지</h1>
+            </div>
+        </>
+    )
+}
+
+export default Mypage;
---
+++ frontend/src/assets/rankPage/Rank.jsx
@@ -0,0 +1,12 @@
+
+const Rank = function(){
+    return(
+        <>
+            <div>
+                <h1>랭킹 페이지</h1>
+            </div>
+        </>
+    )
+}
+
+export default Rank;
