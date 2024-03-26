ac2100106e3ed79f346b81fcac479f4987ccf7c3
sehyun <germs1020@gmail.com>
2024-03-27 03:33:11 +0900

    feat: Calendar 컴포넌트 추가

--- frontend/src/assets/myPage/Mypage.jsx
+++ frontend/src/assets/myPage/Mypage.jsx
@@ -1,9 +1,11 @@
+import Calendar from "../common/calendar/Calendar";
 
 const Mypage = function(){
     return(
         <>
             <div>
                 <h1>마이 페이지</h1>
+                <Calendar/>
             </div>
         </>
     )
