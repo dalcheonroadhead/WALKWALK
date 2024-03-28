e3e2b2420b74f772b1f41d72686b31c9ec154173
rlarbfl0702 <rlarbfl0702@naver.com>
2024-03-29 02:50:09 +0900

    feat: 사이드바 구현 및 사이드바로 페이지 이동 구현

---
+++ frontend/src/assets/yesterdayPage/Yesterday.jsx
@@ -0,0 +1,13 @@
+import styles from "./Yesterday.module.css"
+
+const Yesterday = function(){
+    return(
+        <>
+            <div className={styles.main_container}>
+                <h1>어제의 나 페이지</h1>
+            </div>
+        </>
+    )
+}
+
+export default Yesterday;
