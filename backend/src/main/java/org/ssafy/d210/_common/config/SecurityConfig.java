3155b082fbeac5711df40a0dde820f7dc5dd6b24
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-03-29 09:37:34 +0900

    FIX: 에러를 보기 위해, 잠시 에러 헨들러 끔

--- backend/src/main/java/org/ssafy/d210/_common/config/SecurityConfig.java
+++ backend/src/main/java/org/ssafy/d210/_common/config/SecurityConfig.java
@@ -93,9 +93,9 @@ public class SecurityConfig {
                                         .anyRequest().authenticated()
 
                 )
-                .exceptionHandling(authentication ->        // 7)
-                        authentication.authenticationEntryPoint(authenticationEntryPoint)
-                                .accessDeniedHandler(accessDeniedHandler))
+//                .exceptionHandling(authentication ->        // 7)
+//                        authentication.authenticationEntryPoint(authenticationEntryPoint)
+//                                .accessDeniedHandler(accessDeniedHandler))
 
                                                             // 8)
                .addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
