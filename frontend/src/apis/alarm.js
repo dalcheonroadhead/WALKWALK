b2ece6e7900390b28af74b0ad69c63a18d6072f5
sehyun <germs1020@gmail.com>
2024-04-03 01:17:43 +0900

    fix: 알림 로직 수정

--- frontend/src/apis/alarm.js
+++ frontend/src/apis/alarm.js
@@ -1,17 +1,5 @@
 import { instance } from "./axiosModule";
 
-// 내 운동기준 정보 조회
-// export const getAlarmList = async () => {
-//     const url = `/walk/criteria`;
-    
-//     return await instance.get(url)
-//         .then((res) => {
-//             console.log(res.data.data);
-//             return res.data.data;
-//         })
-//         .catch((err) => {console.log(err)})
-// }
-
 // 알림 목록 조회
 export const getAlarmList = async () => {
     const url = `/notifications/`;
