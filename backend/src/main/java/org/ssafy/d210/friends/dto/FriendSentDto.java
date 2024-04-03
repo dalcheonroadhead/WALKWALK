54ee2fb112e7005a98b913318a69ebcd52958d94
sehyun <germs1020@gmail.com>
2024-04-03 15:45:46 +0900

    fix: 친구 목록 조회시 한줄 소개도 같이 반환하도록 수정

--- backend/src/main/java/org/ssafy/d210/friends/dto/FriendSentDto.java
+++ backend/src/main/java/org/ssafy/d210/friends/dto/FriendSentDto.java
@@ -9,12 +9,14 @@ public class FriendSentDto {
     private String profileUrl;
     private String nickname;
     private Long memberId;
+    private String comment;
 
     @Builder
-    private FriendSentDto(String profileUrl, String nickname, Long memberId){
+    private FriendSentDto(String profileUrl, String nickname, Long memberId, String comment){
         this.profileUrl = profileUrl;
         this.nickname = nickname;
         this.memberId = memberId;
+        this.comment = comment;
     }
 
     public static FriendSentDto from(FriendList friendList){
@@ -22,6 +24,7 @@ public class FriendSentDto {
                 .profileUrl(friendList.getReceiverId().getProfileUrl())
                 .nickname(friendList.getReceiverId().getNickname())
                 .memberId(friendList.getReceiverId().getId())
+                .comment(friendList.getReceiverId().getComment())
                 .build();
     }
 }
